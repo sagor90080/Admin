3a3d60b67dfa202b3cf892f7960b8a1f7323b1bf
Md Sagor <ytback26@gmail.com>
2025-12-10 01:09:10 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,20 +4,18 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.9.1/firebase
 import { getDatabase } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCUSrGAF-k4R3xEXuRwfqHGmOVUD4ty-JU",                 // your api key
-  authDomain: "https://github.com/sagor90080/Admin",         // your auth domain e.g. your-app.firebaseapp.com
-  databaseURL: "https://live-tournament-866fc-default-rtdb.firebaseio.com",      // e.g. https://your-app-default-rtdb.firebaseio.com
-  projectId: "live-tournament-866fc",           // your firebase project id
-  storageBucket: "live-tournament-866fc",   // e.g. your-app.appspot.com
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID_HERE",
-  appId: "YOUR_APP_ID_HERE",
-  measurementId: "YOUR_MEASUREMENT_ID_HERE"
+  apiKey: "AIzaSyAtobuqhCLiDIeFmzc8f6ZGupDui5e2iuY",
+  authDomain: "admin-8bfb6.firebaseapp.com",
+  databaseURL: "https://admin-8bfb6-default-rtdb.firebaseio.com",
+  projectId: "admin-8bfb6",
+  storageBucket: "admin-8bfb6.firebasestorage.app",
+  messagingSenderId: "432275747826",
+  appId: "1:432275747826:web:1eec3e1b1ac60e4316716b",
+  measurementId: "G-84HSP7VJ41"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const db = getDatabase(app);
-
 // Export the initialized services
 export { app, analytics, db };
