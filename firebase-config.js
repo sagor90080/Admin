9339f5ef262424d689069acb706b0898f1409caf
Md Sagor <ytback26@gmail.com>
2025-12-10 01:18:06 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,11 +4,11 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.9.1/firebase
 import { getDatabase } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAtobuqhCLiDIeFmzc8f6ZGupDui5e2iuY",
-  authDomain: "admin-8bfb6.firebaseapp.com",
-  databaseURL: "https://admin-8bfb6-default-rtdb.firebaseio.com",
-  projectId: "admin-8bfb6",
-  storageBucket: "admin-8bfb6.firebasestorage.app",
+  apiKey: "AIzaSyAtobuqhCLiDIeFmzc8f6ZGupDui5e2iuY",                 // your api key
+  authDomain: "admin-8bfb6.firebaseapp.com",         // your auth domain e.g. your-app.firebaseapp.com
+  databaseURL: "https://admin-8bfb6-default-rtdb.firebaseio.com",      // e.g. https://your-app-default-rtdb.firebaseio.com
+  projectId: "admin-8bfb6",           // your firebase project id
+  storageBucket: "admin-8bfb6.firebasestorage.app",   // e.g. your-app.appspot.com
   messagingSenderId: "432275747826",
   appId: "1:432275747826:web:1eec3e1b1ac60e4316716b",
   measurementId: "G-84HSP7VJ41"
@@ -17,5 +17,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getDatabase(app);
+
 // Export the initialized services
 export { app, analytics, db };
